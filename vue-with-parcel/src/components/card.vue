164bc629b39c1f204e08d7f8c57be8722d5e1a2e
Faisal <aryuical@gmail.com>
2019-11-08 15:33:03 +0700

    Finishing Testing

--- vue-with-parcel/src/components/card.vue
+++ vue-with-parcel/src/components/card.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
     <div class="card mb-3">
-      <img :src="picture" class="card-img-top" alt height="300" width="100" />
+      <img
+        :src="picture.image"
+        :img="picture.image"
+        class="card-img-top"
+        alt
+        height="300"
+        width="100"
+      />
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
-        <p
-          class="card-text"
-        >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text">ini gambar</p>
         <p class="card-text">
           <small class="text-muted">Last updated 3 mins ago</small>
         </p>
@@ -29,7 +34,7 @@ export default {
   data() {
     return {
       img: "",
-      shareFb: `http://www.facebook.com/sharer.php?u=${this.img}`
+      shareFb: `http://www.facebook.com/sharer.php?u=${this.picture.image}`
     };
   },
   props: ["picture"]
